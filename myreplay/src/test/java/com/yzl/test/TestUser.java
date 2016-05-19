e6d64803f9d2118e637b3fbcdab236e95bd80421
yzlon <yzlon@qq.com>
2016-05-19 10:07:00 +0800

    first

--- myreplay/src/test/java/com/yzl/test/TestUser.java
+++ myreplay/src/test/java/com/yzl/test/TestUser.java
@@ -2,20 +2,15 @@ package com.yzl.test;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "classpath:conf/applicationContext.xml" })
 public class TestUser {
-	@Autowired
-	private User user = null;
 
 	@Test
 	public void testUser() {
-		user.setName("yzl");
-		System.out.println(user.getName());
 	}
 
 }
